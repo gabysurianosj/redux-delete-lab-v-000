e7e8b3bcd87e42fbb46839148cedffcbc459ed75
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-06 08:44:18 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -4,7 +4,7 @@ export default function manageBand(state = {
   switch (action.type) {
     case 'ADD_BAND':
       const band = { id: Math.random(), name: action.name}
-            return { ...state, bands: [...state.bands, action.name] }	      return { ...state, bands: [...state.bands, band] }
+            return { ...state, bands: [...state.bands, action.name] }	      
           case 'DELETE_BAND':
 
             return {bands: state.bands.filter(band => band.id !== action.id)}
