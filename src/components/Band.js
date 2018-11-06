5c17696bcd6e943bf01384d95c8a6da609a269a3
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-06 08:43:30 +0000

    automatically backed up by learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -9,7 +9,10 @@ class Band extends Component {
   render() {
     return(
       <div>
-        Band Component
+      <li>{this.props.name}</li>
+        <button onClick={this.handleOnClick}>
+          Delete
+        </button>
       </div>
     );
   }
